cb816d07cf7685b7caba5e951d28b816e799a626
Giannis Prokopiou <johnprok89@gmail.com>
2025-02-07 02:00:00 +0200

    Replacing wrong front end

--- front/final-project-1st-attempt/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-import { UserComponent } from './app/user/user.component';
-
-bootstrapApplication(UserComponent, {
-    providers: [provideHttpClient()]
-}).catch((err) => console.error(err));
